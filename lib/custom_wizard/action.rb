202a202caf6abfb80f039e1919323f564ff6506f
Stefano Sisti <s.sisti@elogic.it>
2026-06-24 16:07:58 +0200

    FEAT: add Italian translations for time groups and validation messages in action.rb and client.it.yml

--- lib/custom_wizard/action.rb
+++ lib/custom_wizard/action.rb
@@ -6,9 +6,6 @@ class CustomWizard::Action
 
   WIZARD_USER = "wizard-user"
 
-  # commento di prova inserito
-  # commento di prova inserito 2
-
   def initialize(opts)
     @wizard = opts[:wizard]
     @action = opts[:action]
